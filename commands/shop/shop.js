5bf16b41c1b857bd5919cddd06cf9ba89c481060
Cameron Howell <crh0043@uah.edu>
2020-08-04 11:17:05 -0500

    Fixed async error in shop command

--- commands/shop/shop.js
+++ commands/shop/shop.js
@@ -3,12 +3,12 @@ const { CurrencyShop } = require("../../dbObjects");
 module.exports = {
 	name: "shop",
 	category: "shop",
-    description: "Displays items currently in the shop",
-    run: (message) => {
-        const items = await CurrencyShop.findAll();
-        return message.channel.send(
-            items.map((item) => `${item.name}: ${item.cost}`).join("\n"),
-            { code: true }
-        );
-    }
+	description: "Displays items currently in the shop",
+	run: async (message) => {
+		const items = await CurrencyShop.findAll();
+		return message.channel.send(
+			items.map((item) => `${item.name}: ${item.cost}`).join("\n"),
+			{ code: true }
+		);
+	},
 };
