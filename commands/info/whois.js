2cc425a9446e7d6492d8b259f5cdf60f0a8f727b
Cameron Howell <crh0043@uah.edu>
2020-08-04 22:14:43 -0500

    Converted shop.js and whois.js to ES6 module

--- commands/info/whois.js
+++ commands/info/whois.js
@@ -2,54 +2,50 @@ const { MessageEmbed } = require("discord.js");
 const { stripIndents } = require("common-tags");
 const { getMember, formatDate } = require("../../functions.js");
 
-module.exports = {
-	name: "whois",
-	aliases: ["who", "user", "info"],
-	category: "info",
-	description: "Returns user information",
-	usage: "[username | id | mention]",
-	run: (client, message, args) => {
-		const member = getMember(message, args.join(" "));
+export const name = "whois";
+export const aliases = ["who", "user", "info"];
+export const category = "info";
+export const description = "Returns user information";
+export const usage = "[username | id | mention]";
+export function run(client, message, args) {
+	const member = getMember(message, args.join(" "));
 
-		const joined = formatDate(member.joinedAt);
-		const roles =
-			member.roles.cache
-				.filter((r) => r.id !== message.guild.id)
-				.map((r) => r)
-				.join(", ") || "none";
+	const joined = formatDate(member.joinedAt);
+	const roles =
+		member.roles.cache
+			.filter((r) => r.id !== message.guild.id)
+			.map((r) => r)
+			.join(", ") || "none";
 
-		const created = formatDate(member.user.createdAt);
+	const created = formatDate(member.user.createdAt);
 
-		const embed = new MessageEmbed()
-			.setFooter(member.displayName, member.user.displayAvatarURL)
-			.setThumbnail(member.user.displayAvatarURL)
-			.setColor(
-				member.displayHexColor === "#000000"
-					? "#ffffff"
-					: member.displayHexColor
-			)
-			.addField(
-				"Member information:",
-				stripIndents`**> Display name:** ${member.displayName}
+	const embed = new MessageEmbed()
+		.setFooter(member.displayName, member.user.displayAvatarURL)
+		.setThumbnail(member.user.displayAvatarURL)
+		.setColor(
+			member.displayHexColor === "#000000" ? "#ffffff" : member.displayHexColor
+		)
+		.addField(
+			"Member information:",
+			stripIndents`**> Display name:** ${member.displayName}
 			**> Joined at:** ${joined}
 			**> Roles:** ${roles}`,
-				true
-			)
-			.addField(
-				"User information:",
-				stripIndents`**> ID**: ${member.user.id}
+			true
+		)
+		.addField(
+			"User information:",
+			stripIndents`**> ID**: ${member.user.id}
 			**> Username**: ${member.user.username}
 			**> Tag**: ${member.user.tag}
 			**> Created at**: ${created}`
-			)
-			.setTimestamp();
+		)
+		.setTimestamp();
 
-		if (member.user.presence.activities.length > 0)
-			embed.addField(
-				"Currently playing",
-				stripIndents`**> Name:** ${member.user.presence.activities[0].name}`
-			);
+	if (member.user.presence.activities.length > 0)
+		embed.addField(
+			"Currently playing",
+			stripIndents`**> Name:** ${member.user.presence.activities[0].name}`
+		);
 
-		message.channel.send(embed);
-	},
-};
+	message.channel.send(embed);
+}
--- commands/shop/shop.js
+++ commands/shop/shop.js
@@ -1,14 +1,11 @@
 const { CurrencyShop } = require("../../dbObjects");
-
-module.exports = {
-	name: "shop",
-	category: "shop",
-	description: "Displays items currently in the shop",
-	run: async (client, message) => {
-		const items = await CurrencyShop.findAll();
-		return message.channel.send(
-			items.map((item) => `${item.name}: ${item.cost}`).join("\n"),
-			{ code: true }
-		);
-	},
-};
+export const name = "shop";
+export const category = "shop";
+export const description = "Displays items currently in the shop";
+export async function run(client, message) {
+	const items = await CurrencyShop.findAll();
+	return message.channel.send(
+		items.map((item) => `${item.name}: ${item.cost}`).join("\n"),
+		{ code: true }
+	);
+}
