22fbdcf098b156f88cc93f041ecddc268c233ef0
Cameron Howell <crh0043@uah.edu>
2020-08-05 00:54:59 -0500

    Fixed modules

--- commands/shop/shop.js
+++ commands/shop/shop.js
@@ -1,11 +1,13 @@
 const { CurrencyShop } = require("../../dbObjects");
-export const name = "shop";
-export const category = "shop";
-export const description = "Displays items currently in the shop";
-export async function run(client, message) {
-	const items = await CurrencyShop.findAll();
-	return message.channel.send(
-		items.map((item) => `${item.name}: ${item.cost}`).join("\n"),
-		{ code: true }
-	);
-}
+module.exports = {
+	name: "shop",
+	category: "shop",
+	description: "Displays items currently in the shop",
+	run: async (client, message) => {
+		const items = await CurrencyShop.findAll();
+		return message.channel.send(
+			items.map((item) => `${item.name}: ${item.cost}`).join("\n"),
+			{ code: true }
+		);
+	},
+};
