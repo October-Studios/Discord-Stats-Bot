a125d71fba83c33e6c0d5cf55789027f79a28366
Cameron Howell <crh0043@uah.edu>
2020-08-04 22:23:27 -0500

    Converted shop.js and whois.js to ES6 module

--- commands/shop/leaderboard.js
+++ commands/shop/leaderboard.js
@@ -1,19 +1,21 @@
-export const name = "leaderboard";
-export const category = "shop";
-export const description = "Display currency standings";
-export function run(client, message) {
-	return message.channel.send(
-		currency
-			.sort((a, b) => b.balance - a.balance)
-			.filter((user) => client.users.cache.has(user.user_id))
-			.first(10)
-			.map(
-				(user, position) =>
-					`${position + 1} ${client.users.cache.get(user.user_id).tag}: ${
-						user.balance
-					}`
-			)
-			.join("\n"),
-		{ code: true }
-	);
-}
+module.exports = {
+	name: "leaderboard",
+	category: "shop",
+	description: "Display currency standings",
+	run: (client, message) => {
+		return message.channel.send(
+			currency
+				.sort((a, b) => b.balance - a.balance)
+				.filter((user) => client.users.cache.has(user.user_id))
+				.first(10)
+				.map(
+					(user, position) =>
+						`${position + 1} ${client.users.cache.get(user.user_id).tag}: ${
+							user.balance
+						}`
+				)
+				.join("\n"),
+			{ code: true }
+		);
+	},
+};
