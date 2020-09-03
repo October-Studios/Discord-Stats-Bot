023799f1a5400bb0693281e09b2924dfdff3a7b2
Cameron Howell <crh0043@uah.edu>
2020-09-02 23:17:16 -0500

    Extended functionality to robin

--- commands/info/robin.js
+++ commands/info/robin.js
@@ -1,13 +1,22 @@
-const {PythonShell} = require("python-shell");
-
+const { PythonShell } = require("python-shell");
+let pyshell = new PythonShell("/home/ubuntu/Discord-Stats-Bot/stocks.py");
 module.exports = {
 	name: "robin",
 	category: "info",
 	description: "Gives information about current stock market data",
 	run: async (client, message, args) => {
-    PythonShell.run('/home/ubuntu/Discord-Stats-Bot/stocks.py', null, function (err, results) {
-			if (err) throw err
-			return message.channel.send(results)
-    });
+		if (!args[0]) {
+			return message.channel.send("Please specify a function!");
+		}
+		if (!args[1]) {
+			return message.channel.send("Please specify an entity!");
+		}
+		if (args[0].toLowerCase === "value" && args[1].toLowerCase() === "cam") {
+			pyshell.send("crhowell3@gmail.com HOWELL_ROBIN");
+		}
+		await pyshell.stdout(async function (err, results) {
+			if (err) throw err;
+			return message.channel.send(results);
+		});
 	},
 };
--- stocks.py
+++ stocks.py
@@ -2,15 +2,16 @@ import robin_stocks as r
 import os
 import sys
 
-password = os.environ.get("ROBINHOOD")
+username = sys.stdin[0]
+password = os.environ.get(sys.stdin[1])
 
 
 def main():
-    login = r.login("crhowell3@gmail.com", password)
+    login = r.login(username, password)
     my_stocks = r.build_holdings()
     for key, value in my_stocks.items():
-      print(key, value)
-      sys.stdout.flush()
+        print(key, value)
+        sys.stdout.flush()
 
 
 if __name__ == "__main__":
