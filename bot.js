628c9d30255b8aa4b3f0acbcffbcef816d01c078
Cameron Howell <crh0043@uah.edu>
2020-09-07 01:13:46 -0500

    Moved inventory command to new format

--- bot.js
+++ bot.js
@@ -82,19 +82,7 @@ client.on("message", async (message) => {
 		cmd.run(client, message, args);
 	}
 
-	if (command === "inventory") {
-		const target = message.mentions.users.first() || message.author;
-		const user = await Users.findOne({ where: { user_id: target.id } });
-		const items = await user.getItems();
-
-		if (!items.length)
-			return message.channel.send(`${target.tag} has nothing!`);
-		return message.channel.send(
-			`${target.tag} currently has ${items
-				.map((i) => `${i.amount} ${i.item.name}`)
-				.join(", ")}`
-		);
-	} else if (command === "transfer") {
+	if (command === "transfer") {
 		const currentAmount = currency.getBalance(message.author.id);
 		const transferAmount =
 			commandArgs.split(/ +/g) / find((arg) => !/<@!?\d+>/g.test(arg));
