1f9582d7b98aa3f27d107296ed295a8a49121272
Cameron Howell <crh0043@uah.edu>
2020-09-07 01:33:44 -0500

    Removed transfer command from bot.js

--- bot.js
+++ bot.js
@@ -82,33 +82,7 @@ client.on("message", async (message) => {
 		cmd.run(client, message, args);
 	}
 
-	if (command === "transfer") {
-		const currentAmount = currency.getBalance(message.author.id);
-		const transferAmount =
-			commandArgs.split(/ +/g) / find((arg) => !/<@!?\d+>/g.test(arg));
-		const transferTarget = message.mentions.users.first();
-
-		if (!transferAmount || isNaN(transferAmount))
-			return message.channel.send(
-				`Sorry ${message.author}, that's an invalid amount.`
-			);
-		if (transferAmount > currentAmount)
-			return message.channel.send(
-				`Sorry ${message.author}, you only have ${currentAmount}.`
-			);
-		if (transferAmount <= 0)
-			return message.channel.send(
-				`Please enter an amount greater than zero, ${message.author}.`
-			);
-		currency.add(message.author.id, -transferAmount);
-		currency.add(transferTarget.id, transferAmount);
-
-		return message.channel.send(
-			`Successfully transferred ${transferAmount} to ${
-				transferTarget.tag
-			}. Your current balance is ${currency.getBalance(message.author.id)}`
-		);
-	} else if (command === "buy") {
+	if (command === "buy") {
 		const item = await CurrencyShop.findOne({
 			where: { name: { [Op.like]: args[0] } },
 		});
