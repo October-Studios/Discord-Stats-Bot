df84f055e9f9cc066b7c4c38aa3e8ed248a9f742
Cameron Howell <crh0043@uah.edu>
2020-09-07 22:01:19 -0500

    Removed value user for now

--- commands/info/robin.js
+++ commands/info/robin.js
@@ -2,43 +2,25 @@ const { PythonShell } = require("python-shell");
 var options = {
 	scriptPath: "/home/ubuntu/Discord-Stats-Bot",
 };
-var username = "";
-var password = "";
+var username = "crhowell3@gmail.com";
+var password = "HOWELL_ROBIN";
 
 module.exports = {
 	name: "robin",
 	category: "info",
 	description: "Gives information about current stock market data",
-	usage: "<value | stock> <user | stock_name>",
+	usage: "<symbol>",
 	run: async (client, message, args) => {
 		if (!args[0]) {
 			return message.channel.send("Please specify a function!");
 		}
-		if (!args[1]) {
-			return message.channel.send("Please specify an entity!");
-		}
-		if (args[0].toLowerCase() === "value") {
-			if (args[1].toLowerCase() === "cam") {
-				username = "crhowell3@gmail.com";
-				password = "HOWELL_ROBIN";
-			} else return message.channel.send("Invalid person!");
-			var pyshell = new PythonShell("stocks.py", options, { mode: "text" });
-			pyshell.send(username + " " + password);
-			pyshell.stdout.on("data", function (data) {
-				message.channel.send(data);
-			});
-			pyshell.end(function (err) {
-				if (err) throw err;
-			});
-		} else if (args[0].toLowerCase() === "stock") {
-			var pyshell = new PythonShell("stocks.py", options, { mode: "text" });
-			pyshell.send(username + " " + password + " " + args[1]);
-			pyshell.stdout.on("data", function (data) {
-				message.channel.send(data);
-			});
-			pyshell.end(function (err) {
-				if (err) throw err;
-			});
-		}
+		var pyshell = new PythonShell("stocks.py", options, { mode: "text" });
+		pyshell.send(username + " " + password + " " + args[0]);
+		pyshell.stdout.on("data", function (data) {
+			message.channel.send(data);
+		});
+		pyshell.end(function (err) {
+			if (err) throw err;
+		});
 	},
 };
