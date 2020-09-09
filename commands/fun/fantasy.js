1b8458528f0fb01dc0e4decf42cfc0870bb9a5d3
Cameron Howell <crh0043@uah.edu>
2020-09-09 22:11:47 +0000

    Added more arguments

--- commands/fun/fantasy.js
+++ commands/fun/fantasy.js
@@ -9,7 +9,7 @@ module.exports = {
 	name: "fantasy",
 	category: "fun",
 	description: "Get stats for the 2020 Fantasy League",
-	usage: "<integer>",
+	usage: "<team abbr | 'all' | 'list'>",
 	run: async (client, message, args) => {
 		myClient = new Client({
 			leagueId,
@@ -17,9 +17,13 @@ module.exports = {
 			SWID: process.env.SWID,
 		});
 		let teams = await myClient.getTeamsAtWeek({ seasonId, scoringPeriodId: 1 });
+		if (!args[0]){
+
+    } else if (args[0] == 'list') {
+    	return message.channel.send(`${teams[0].name} (${teams[0].abbreviation})\n${teams[1].name} (${teams[1].abbreviation})\n${teams[2].name} (${teams[2].abbreviation})\n${teams[3].name} (${teams[3].abbreviation})`)
+    }
 		const embed = new MessageEmbed()
 			.setColor("#0db14b")
-			.setThumbnail(`${teams[0].logoURL}`)
 			.setTitle(
 				`${teams[0].name} (${teams[0].wins}-${teams[0].losses}-${teams[0].ties})`
 			)
