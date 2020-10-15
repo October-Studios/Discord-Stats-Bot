9d73fedc9016ca68f7857303493ef6ad5768bc18
Cameron Howell <crh0043@uah.edu>
2020-10-15 00:23:00 -0500

    Who even knows

--- commands/general/ticket.js
+++ commands/general/ticket.js
@@ -4,6 +4,7 @@ module.exports = {
 	description: "Put in a help ticket",
 	usage: "<string>",
 	run: (client, message, args) => {
+        let userMessage = message;
         const filter = (reaction, user) => reaction.emoji.name === '✅' && user.id === '396178060187271169';
         client.channels.cache.get('751922155217748030').send(`Ticket created by ${message.author}:\n\n${args.join(' ')}\n\nClick the ✅ icon when this issue has been resolved.`)
             .then(message => {
