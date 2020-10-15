e361155de2280da7a8ccdf76887f5b55111c35c8
Cameron Howell <crh0043@uah.edu>
2020-10-15 00:38:26 -0500

    >:(

--- commands/general/ticket.js
+++ commands/general/ticket.js
@@ -5,22 +5,20 @@ module.exports = {
 	usage: "<string>",
 	run: (client, message, args) => {
         let userMessage = message;
-        let filter = ('messageReactionAdd', (reaction, user) => {
+        let filter = (reaction, user) => {
             if(user.bot) return false;
             if(reaction.emoji.name === '✅' && user.id === '396178060187271169') {
                 return true;
             }
-        });
+        };
         client.channels.cache.get('751922155217748030').send(`Ticket created by ${message.author}:\n\n${args.join(' ')}\n\nClick the ✅ icon when this issue has been resolved.`)
             .then(message => {
                 message.react('✅');
                 userMessage.reply(
                     'Ticket received! Expect reply in 24 hours.'
                 );
-                message.awaitReactions(filter)
-                    .then(c => {
-                        message.delete();
-                    });
+                const collector = message.createReactionCollector(filter);
+                collector.on('collect', r => message.delete());
             });
 
     }
