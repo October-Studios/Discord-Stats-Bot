d29f9e56c24f55deb477e26093e0ec9aee9d5273
Cameron Howell <crh0043@uah.edu>
2020-10-15 00:33:09 -0500

    idek im mad

--- commands/general/ticket.js
+++ commands/general/ticket.js
@@ -5,7 +5,12 @@ module.exports = {
 	usage: "<string>",
 	run: (client, message, args) => {
         let userMessage = message;
-        const filter = (reaction, user) => reaction.emoji.name === '✅' && user.id === '396178060187271169';
+        let filter = (reaction, user) => {
+            if(user.bot) return false;
+            if(reaction.emoji.name === '✅' && user.id === '396178060187271169') {
+                return true;
+            }
+        };
         client.channels.cache.get('751922155217748030').send(`Ticket created by ${message.author}:\n\n${args.join(' ')}\n\nClick the ✅ icon when this issue has been resolved.`)
             .then(message => {
                 message.react('✅');
