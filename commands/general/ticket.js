2f9b844e7ff8ed2f38d7d844b976039cf79dc7aa
Cameron Howell <crh0043@uah.edu>
2020-10-15 00:04:11 -0500

    More ticket info

--- commands/general/ticket.js
+++ commands/general/ticket.js
@@ -4,8 +4,8 @@ module.exports = {
 	name: "ticket",
 	category: "general",
 	description: "Put in a help ticket",
-	usage: "<inquiry>",
+	usage: "<string>",
 	run: (client, message, args) => {
-        client.channel.fetch('751922155217748030').send(`Ticket created by ${message.author}`);
+        client.channel.fetch('751922155217748030').send(`Ticket created by ${message.author}:\n\n${args}`);
     }
 };
