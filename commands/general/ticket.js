395a75eac165da550233907995e20ed7d711996a
Cameron Howell <crh0043@uah.edu>
2020-10-14 23:58:57 -0500

    Sending message to right channel ?

--- commands/general/ticket.js
+++ commands/general/ticket.js
@@ -1,9 +1,11 @@
+const { Guild } = require("discord.js");
+
 module.exports = {
 	name: "ticket",
 	category: "general",
 	description: "Put in a help ticket",
 	usage: "<inquiry>",
 	run: (client, message, args) => {
-        message.channel.send(`Ticket created by ${message.author}`);
+        client.channel.fetch('751922155217748030').send(`Ticket created by ${message.author}`);
     }
 };
