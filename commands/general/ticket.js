9a36221e2c8671c7cc80d7847c5bec2d099f55df
Cameron Howell <crh0043@uah.edu>
2020-10-15 05:04:30 +0000

    Concurrency

--- commands/general/ticket.js
+++ commands/general/ticket.js
@@ -1,11 +1,9 @@
-const { Guild } = require("discord.js");
-
 module.exports = {
 	name: "ticket",
 	category: "general",
 	description: "Put in a help ticket",
 	usage: "<inquiry>",
 	run: (client, message, args) => {
-        client.channel.fetch('751922155217748030').send(`Ticket created by ${message.author}`);
+        client.channels.cache.get('751922155217748030').send(`Ticket created by ${message.author}`);
     }
 };
