93dfba9ffcdd6549221e45d3b5d77ae33cf4f488
Cameron Howell <crh0043@uah.edu>
2020-10-15 00:19:48 -0500

    Big test

--- commands/general/ticket.js
+++ commands/general/ticket.js
@@ -4,7 +4,18 @@ module.exports = {
 	description: "Put in a help ticket",
 	usage: "<string>",
 	run: (client, message, args) => {
-        client.channels.cache.get('751922155217748030').send(`Ticket created by ${message.author}:\n\n${args.join(' ')}`);
+        const filter = (reaction, user) => reaction.emoji.name === '✅' && user.id === '396178060187271169';
+        client.channels.cache.get('751922155217748030').send(`Ticket created by ${message.author}:\n\n${args.join(' ')}\n\nClick the ✅ icon when this issue has been resolved.`)
+            .then(message => {
+                message.react('✅');
+                userMessage.reply(
+                    'Ticket received! Expect reply in 24 hours.'
+                );
+                message.awaitReactions(filter)
+                    .then(collected => {
+                        message.delete();
+                    });
+            });
 
     }
 };
