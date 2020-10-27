e6e91f29a0461126f64a2723d807bb549aaccc41
Cameron Howell <crh0043@uah.edu>
2020-10-27 13:02:45 -0500

    Increased roll limit to MAX_SAFE_INTEGER

--- commands/fun/roll.js
+++ commands/fun/roll.js
@@ -10,9 +10,9 @@ module.exports = {
 			return message.channel.send("Too many arguments.");
 		} else if (!parseInt(args[0]) && parseInt(args[0]) != 0) {
 			return message.channel.send("You must use an integer value!");
-		} else if (parseInt(args[0]) > 100000000 || parseInt(args[0]) < 1) {
+		} else if (parseInt(args[0]) > Number.MAX_SAFE_INTEGER || parseInt(args[0]) < 1) {
 			return message.channel.send(
-				"Please use a value between 1 and 100,000,000!)"
+				"Please use a value between 1 and 9007199254740991!)"
 			);
 		} else {
 			return message.channel.send(Math.ceil(Math.random() * parseInt(args[0])));
