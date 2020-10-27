fb60ef82160a6e7dbcc04110e9767fd13d51470f
Cameron Howell <crh0043@uah.edu>
2020-10-27 19:22:49 +0000

    Fixed precision errors

--- commands/general/days_until.js
+++ commands/general/days_until.js
@@ -10,18 +10,19 @@ module.exports = {
         } else if (!(/^[0-1][0-9]\/([0-2][0-9]|3[0-1])\/[0-9]{4}$/.test(args[0]))) {
             return message.channel.send("Invalid date format! Please use mm/dd/yyyy");
         } else {
-            var day1 = new Date.now();
+            var temp = Date.now();
+            var day1 = new Date(temp);
             var day2 = new Date(args[0]);
 
-            var time_diff = day1 - day2.getTime();
+            var time_diff = day2.getTime() - day1.getTime();
             var day_diff = time_diff / (1000 * 3600 * 24);
 
-            if (day_diff > 0) {
-                return message.channel.send("There are " + day_diff + " days until " + args[0]);
-            } else if (day_diff == 0) {
+            if (Math.ceil(day_diff) > 0) {
+                return message.channel.send("There are " + Math.ceil(day_diff) + " days until " + args[0]);
+            } else if (Math.ceil(day_diff) == 0) {
                 return message.channel.send("That's today!");
-            } else if (day_diff < 0) {
-                return message.channel.send(args[0] + " was " + (day_diff * -1) + " days ago!");
+            } else if (Math.ceil(day_diff) < 0) {
+                return message.channel.send(args[0] + " was " + Math.ceil(day_diff * -1) + " days ago!");
             }
         }
     }
