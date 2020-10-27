32934ac8df610d1360098a18e401c222bb01a0fe
Cameron Howell <crh0043@uah.edu>
2020-10-27 13:17:10 -0500

    Added arguments to about

--- commands/info/about.js
+++ commands/info/about.js
@@ -7,23 +7,32 @@ module.exports = {
 	aliases: ["a"],
 	category: "info",
 	description: "Provides information about me!",
-	run: async (client, message) => {
+	usage: "[total | name | version]",
+	run: async (client, message, args) => {
 		let adapter = new FileSync('db.json');
 		let db = low(adapter);
-		return message.channel.send(
-			"Bot name: " +
-				name + "(カーリー)" +
-				"\nRelease Version: v" +
-				version +
-				"\nCreator: " +
-				author +
-				"\nDescription: " +
-				description +
-				"\nRelease notes v" +
-				version +
-				": Moved all bot commands to new format with command handler and modular structure" +
-				"\nTotal commands run: " + db.get('queried').value()
+		if (args[0] === "total") {
+			return message.channel.send("Total commands run: " + db.get('queried').value());
+		} else if (args[0] === "name") {
+			return message.channel.send("Bot name: " + name + "(カーリー)");
+		}  else if (args[0] === "version") {
+			return message.channel.send("Release Version: v" + version);
+		} else if (!args[0]) {
+			return message.channel.send(
+				"Bot name: " +
+					name + "(カーリー)" +
+					"\nRelease Version: v" +
+					version +
+					"\nCreator: " +
+					author +
+					"\nDescription: " +
+					description +
+					"\nRelease notes v" +
+					version +
+					": Moved all bot commands to new format with command handler and modular structure" +
+					"\nTotal commands run: " + db.get('queried').value()
 
-		);
+			);
+		}
 	},
 };
