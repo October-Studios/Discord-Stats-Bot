bc348fe1c678d574092b988437279b3268fb961f
Cameron Howell <crh0043@uah.edu>
2020-11-03 00:20:56 -0600

    Added election results command

---
+++ commands/election/results.js
@@ -0,0 +1,16 @@
+const axios = require('axios');
+
+module.exports = {
+    name: "results",
+	category: "election",
+	description: "Pulls the current results of the presidential election",
+	run: async (client, message, args) => {
+        axios.get('https://www.270towin.com/2020-election-results-live/')
+        .then((response) => {
+            console.log(response);
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+    },
+};
