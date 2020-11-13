eafd466e4ccb92b24725a6329e629954ba66832e
Cameron Howell <crh0043@uah.edu>
2020-11-12 20:11:21 -0600

    Testing something

--- commands/general/ticket.js
+++ commands/general/ticket.js
@@ -7,19 +7,24 @@ module.exports = {
         let userMessage = message;
         let filter = (reaction, user) => {
             if(user.bot) return false;
-            if(reaction.emoji.name === '✅' && user.id === '396178060187271169') {
+            if((reaction.emoji.name === '✅' || reaction.emoji.name === '❌') && user.id === '396178060187271169') {
                 return true;
             }
         };
         client.channels.cache.get('776496572623224882').send(`Ticket created by ${message.author}:\n\n${args.join(' ')}\n\nClick the ✅ icon when this issue has been resolved.`)
             .then(message => {
                 message.react('✅');
+                message.react('❌');
                 userMessage.reply(
                     'ticket received! Expect reply in 24 hours.'
                 );
                 const collector = message.createReactionCollector(filter);
                 collector.on('collect', r => {
-                    userMessage.author.send("Your ticket has been resolved!");
+                    if (r === '❌') {
+                        message.author.send("Your suggestion has been rejected. If you would like to appeal, PM the owner.");
+                    } else {
+                        userMessage.author.send("Your ticket has been resolved!");
+                    }
                     message.delete();
                 });
             });
