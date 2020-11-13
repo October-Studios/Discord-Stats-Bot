2c630ae07f4a25588c009336e4ee18ed0cc92a96
Cameron Howell <crh0043@uah.edu>
2020-11-12 18:41:13 -0600

    Imported file for getMember function

--- commands/moderation/report.js
+++ commands/moderation/report.js
@@ -1,3 +1,4 @@
+const { getMember } = require("../../functions.js");
 module.exports = {
     name: "report",
     category: "moderation",
@@ -11,9 +12,6 @@ module.exports = {
         }
         if (!args[0] || !args[1]) {
             return message.channel.send("Please provide both a user and a reason for reporting.");
-        }
-        if (!member) {
-            return message.channel.send("This user does not exist in this guild.");
         }
         client.channels.cache.get('776606685442736139').send(`${message.author} reported ${member.user} for ${reason}`);
     },
